81b615816826786b7c3e0d6981520ab215a4210c
unknown <you@example.com>
2023-02-07 15:54:12 +0300

    t

---
+++ Task(6)Layout/Task(6)Layout/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task_6_Layout.Models;
+
+namespace Task_6_Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Customer c= new Customer();
+            ViewBag.massege = c;
+            return View(c);
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            Customer c = new Customer();
+            ViewBag.massege = c;
+            return View(c);
+          
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
