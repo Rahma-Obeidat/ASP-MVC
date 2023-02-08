89370db948f2e7b7e929c0acfba62d3efd7307bd
unknown <you@example.com>
2023-02-08 13:45:57 +0300

    t

---
+++ Forms_Authontication/Forms_Authontication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Forms_Authontication.Startup))]
+namespace Forms_Authontication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
